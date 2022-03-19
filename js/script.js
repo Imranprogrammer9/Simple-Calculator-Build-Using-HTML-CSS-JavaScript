8d320537583e9b504a351cfa398c54d65b1218f5
Imran.Programmer <90908160+Imranprogrammer9@users.noreply.github.com>
2022-03-19 22:18:23 +0500

    Added Some More Functionality

--- js/script.js
+++ js/script.js
@@ -2,23 +2,43 @@
 function sum() {
   var num1 = parseInt(document.getElementById("Num1").value);
   var num2 = parseInt(document.getElementById("Num2").value);
-  document.getElementById("result").value = num1 + num2;
+  if (num1 && num2) {
+    document.getElementById("result").value = num1 + num2;
+  }
+  else {
+    alert("Please enter a number");
+  }
 }
 // Subtraction Function
 function subt() {
   var num1 = parseInt(document.getElementById("Num1").value);
   var num2 = parseInt(document.getElementById("Num2").value);
-  document.getElementById("result").value = num1 - num2;
+  if (num1 && num2) {
+    document.getElementById("result").value = num1 - num2;
+  }
+  else {
+    alert("Please enter a number");
+  }
 }
 // Multiply Function
 function mult() {
   var num1 = parseInt(document.getElementById("Num1").value);
   var num2 = parseInt(document.getElementById("Num2").value);
-  document.getElementById("result").value = num1 * num2;
+  if (num1 && num2) {
+    document.getElementById("result").value = num1 * num2;
+  }
+  else {
+    alert("Please enter a number");
+  }
 }
 // Division Function
 function divide() {
   var num1 = parseInt(document.getElementById("Num1").value);
   var num2 = parseInt(document.getElementById("Num2").value);
-  document.getElementById("result").value = num1 / num2;
+  if (num1 && num2) {
+    document.getElementById("result").value = num1 / num2;
+  }
+  else {
+    alert("Please enter a number");
+  }
 }
